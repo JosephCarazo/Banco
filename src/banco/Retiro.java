342ace19aa9dea4c9ff8efd782cb0c6099e90e02
Francisco Antonio Guzman Rocha <fran556556@gmail.com>
2022-09-08 14:02:03 -0600

    Modifica clase retiro

--- src/banco/Retiro.java
+++ src/banco/Retiro.java
@@ -12,7 +12,15 @@ public class Retiro extends Transaccion {
 
     public Retiro(Cuenta c1,int id, String cuenta, double monto, double saldoAnterior) {
         super(c1,id, cuenta, monto, saldoAnterior);
-        
+        Retiro(c1,monto);
+    }
+    private void Retiro(Cuenta c1 ,double monto){
+        if(monto>c1.getSaldo()){
+            System.out.println("Saldo insuficiente");
+        }else{
+            c1.setSaldo(c1.getSaldo()-monto);
+        }
+            
     }
     
    
