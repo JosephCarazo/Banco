ada3cbe2c46a2d7db3dd611c6edc9c3ee91448dd
Joan Bravo Solano <joansolano100@gmail.com>
2022-09-20 15:01:46 -0600

    Creacion de Paquetes MVC

--- src/Designpatterns/MVC/Controlador/ControladorCuenta.java
+++ src/Designpatterns/MVC/Controlador/ControladorCuenta.java
@@ -4,10 +4,27 @@
  */
 package Designpatterns.MVC.Controlador;
 
+import DesingPatterns.MVC.Vista.VistaConsolaCuenta;
+import banco.Cuenta;
+
 /**
  *
  * @author joans
  */
 public class ControladorCuenta {
+    private VistaConsolaCuenta vista;
+    private Cuenta modelo;
+
+    public void setVista(VistaConsolaCuenta vista) {
+        this.vista = vista;
+    }
+
+    public void setModelo(Cuenta modelo) {
+        this.modelo = modelo;
+    }
+    
+    public void add(){
+    
+    }
     
 }
---
+++ src/Designpatterns/MVC/MODELO/DataBase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Designpatterns.MVC.MODELO;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author joans
+ */
+public class DataBase {
+     private ArrayList<Cuenta> cuentas;
+     
+     public void add(Cuenta cuenta){
+     cuentas.add(cuenta);
+     }
+     public Cuenta Buscar(String numero){
+     for (Cuenta cuenta: cuentas){
+     if(cuenta.getNumeroCuenta().equals(numero)){
+     return cuenta;
+     }
+     }
+     return null;
+     }
+     
+}
